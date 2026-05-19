f9ef7640ccd2e0a8dceb0bc7907a3fbf0298a6c2
Alejandro Quiñones <107212881+AlejandroQuinoneSeco@users.noreply.github.com>
2026-05-19 11:10:03 +0200

    Add files via upload

---
+++ ad-ec-ra1-examen/src/main/java/org/educa/service/ClienteService.java
@@ -0,0 +1,20 @@
+package org.educa.service;
+
+import generated.Clientes;
+import jakarta.xml.bind.JAXBException;
+
+import java.io.IOException;
+
+public class ClienteService {
+
+
+    public Clientes readClientes() throws IOException {
+        //TODO
+        return null;
+    }
+
+
+    public void createXML(Clientes clientes) throws JAXBException, IOException {
+        //TODO
+    }
+}
---
+++ ad-ec-ra1-examen/target/generated-sources/jaxb/generated/Direccion.java
@@ -0,0 +1,155 @@
+//
+// Este archivo ha sido generado por Eclipse Implementation of JAXB v3.0.0 
+// Visite https://eclipse-ee4j.github.io/jaxb-ri 
+// Todas las modificaciones realizadas en este archivo se perderán si se vuelve a compilar el esquema de origen. 
+// Generado el: 2025.10.16 a las 01:26:25 PM CEST 
+//
+
+
+package generated;
+
+import jakarta.xml.bind.annotation.XmlAccessType;
+import jakarta.xml.bind.annotation.XmlAccessorType;
+import jakarta.xml.bind.annotation.XmlElement;
+import jakarta.xml.bind.annotation.XmlRootElement;
+import jakarta.xml.bind.annotation.XmlType;
+
+
+/**
+ * <p>Clase Java para anonymous complex type.
+ * 
+ * <p>El siguiente fragmento de esquema especifica el contenido que se espera que haya en esta clase.
+ * 
+ * <pre>
+ * &lt;complexType&gt;
+ *   &lt;complexContent&gt;
+ *     &lt;restriction base="{http://www.w3.org/2001/XMLSchema}anyType"&gt;
+ *       &lt;sequence&gt;
+ *         &lt;element name="calle" type="{http://www.w3.org/2001/XMLSchema}string"/&gt;
+ *         &lt;element name="ciudad" type="{http://www.w3.org/2001/XMLSchema}string"/&gt;
+ *         &lt;element name="pais" type="{http://www.w3.org/2001/XMLSchema}string"/&gt;
+ *         &lt;element name="cp" type="{http://www.w3.org/2001/XMLSchema}string"/&gt;
+ *       &lt;/sequence&gt;
+ *     &lt;/restriction&gt;
+ *   &lt;/complexContent&gt;
+ * &lt;/complexType&gt;
+ * </pre>
+ * 
+ * 
+ */
+@XmlAccessorType(XmlAccessType.FIELD)
+@XmlType(name = "", propOrder = {
+    "calle",
+    "ciudad",
+    "pais",
+    "cp"
+})
+@XmlRootElement(name = "direccion")
+public class Direccion {
+
+    @XmlElement(required = true)
+    protected String calle;
+    @XmlElement(required = true)
+    protected String ciudad;
+    @XmlElement(required = true)
+    protected String pais;
+    @XmlElement(required = true)
+    protected String cp;
+
+    /**
+     * Obtiene el valor de la propiedad calle.
+     * 
+     * @return
+     *     possible object is
+     *     {@link String }
+     *     
+     */
+    public String getCalle() {
+        return calle;
+    }
+
+    /**
+     * Define el valor de la propiedad calle.
+     * 
+     * @param value
+     *     allowed object is
+     *     {@link String }
+     *     
+     */
+    public void setCalle(String value) {
+        this.calle = value;
+    }
+
+    /**
+     * Obtiene el valor de la propiedad ciudad.
+     * 
+     * @return
+     *     possible object is
+     *     {@link String }
+     *     
+     */
+    public String getCiudad() {
+        return ciudad;
+    }
+
+    /**
+     * Define el valor de la propiedad ciudad.
+     * 
+     * @param value
+     *     allowed object is
+     *     {@link String }
+     *     
+     */
+    public void setCiudad(String value) {
+        this.ciudad = value;
+    }
+
+    /**
+     * Obtiene el valor de la propiedad pais.
+     * 
+     * @return
+     *     possible object is
+     *     {@link String }
+     *     
+     */
+    public String getPais() {
+        return pais;
+    }
+
+    /**
+     * Define el valor de la propiedad pais.
+     * 
+     * @param value
+     *     allowed object is
+     *     {@link String }
+     *     
+     */
+    public void setPais(String value) {
+        this.pais = value;
+    }
+
+    /**
+     * Obtiene el valor de la propiedad cp.
+     * 
+     * @return
+     *     possible object is
+     *     {@link String }
+     *     
+     */
+    public String getCp() {
+        return cp;
+    }
+
+    /**
+     * Define el valor de la propiedad cp.
+     * 
+     * @param value
+     *     allowed object is
+     *     {@link String }
+     *     
+     */
+    public void setCp(String value) {
+        this.cp = value;
+    }
+
+}
