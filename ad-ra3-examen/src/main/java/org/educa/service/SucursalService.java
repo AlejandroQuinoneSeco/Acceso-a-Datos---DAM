84b421d88eb427c67ed172347304c98627dbac5e
Alejandro Quiñones <107212881+AlejandroQuinoneSeco@users.noreply.github.com>
2026-06-04 13:56:51 +0200

    Add files via upload

---
+++ ad-ra3-examen/src/main/java/org/educa/service/SucursalService.java
@@ -0,0 +1,23 @@
+package org.educa.service;
+
+import org.educa.entity.SucursalEntity;
+
+import java.util.List;
+
+public class SucursalService {
+
+    public void insertar(SucursalEntity sucursal) {
+    }
+
+    public SucursalEntity findByCP(String cp) {
+        return null;
+    }
+
+    public Integer deleteByCP(String cp) {
+        return null;
+    }
+
+    public List<SucursalEntity> findByCalle(String texto) {
+        return null;
+    }
+}
