5229d75733f26d1a679e50ee6d680d8f39b42585
Alejandro Quiñones <107212881+AlejandroQuinoneSeco@users.noreply.github.com>
2026-06-11 00:37:29 +0200

    Add files via upload

---
+++ Console.java
@@ -0,0 +1,162 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<project version="4">
+  <component name="AutoImportSettings">
+    <option name="autoReloadType" value="SELECTIVE" />
+  </component>
+  <component name="ChangeListManager">
+    <list default="true" id="0a231334-32e4-40c1-9335-cdd063e03ea5" name="Changes" comment="" />
+    <option name="SHOW_DIALOG" value="false" />
+    <option name="HIGHLIGHT_CONFLICTS" value="true" />
+    <option name="HIGHLIGHT_NON_ACTIVE_CHANGELIST" value="false" />
+    <option name="LAST_RESOLUTION" value="IGNORE" />
+  </component>
+  <component name="FileTemplateManagerImpl">
+    <option name="RECENT_TEMPLATES">
+      <list>
+        <option value="Interface" />
+        <option value="JUnit5 Test Class" />
+        <option value="Class" />
+      </list>
+    </option>
+  </component>
+  <component name="ProjectColorInfo">{
+  &quot;associatedIndex&quot;: 5
+}</component>
+  <component name="ProjectId" id="2sd6iRlLIvPWrdJx9UdQ0aB9jqp" />
+  <component name="ProjectViewState">
+    <option name="autoscrollFromSource" value="true" />
+    <option name="hideEmptyMiddlePackages" value="true" />
+    <option name="showLibraryContents" value="true" />
+  </component>
+  <component name="PropertiesComponent">{
+  &quot;keyToString&quot;: {
+    &quot;RunOnceActivity.OpenProjectViewOnStart&quot;: &quot;true&quot;,
+    &quot;RunOnceActivity.ShowReadmeOnStart&quot;: &quot;true&quot;,
+    &quot;WebServerToolWindowFactoryState&quot;: &quot;false&quot;,
+    &quot;com.intellij.testIntegration.createTest.CreateTestDialog.defaultLibrary&quot;: &quot;JUnit5&quot;,
+    &quot;com.intellij.testIntegration.createTest.CreateTestDialog.defaultLibrarySuperClass.JUnit5&quot;: &quot;&quot;,
+    &quot;full.screen.before.presentation.mode&quot;: &quot;false&quot;,
+    &quot;last_opened_file_path&quot;: &quot;C:/Users/manue/Nextcloud/CLASE/02-DAM-AD/Examenes/2-Ev. Ordinaria/Diurno/RA5/ad-eo-ra5-examen-sol/src/test/java/org/educa&quot;,
+    &quot;node.js.detected.package.eslint&quot;: &quot;true&quot;,
+    &quot;node.js.detected.package.tslint&quot;: &quot;true&quot;,
+    &quot;node.js.selected.package.eslint&quot;: &quot;(autodetect)&quot;,
+    &quot;node.js.selected.package.tslint&quot;: &quot;(autodetect)&quot;,
+    &quot;nodejs_package_manager_path&quot;: &quot;npm&quot;,
+    &quot;project.structure.last.edited&quot;: &quot;Project&quot;,
+    &quot;project.structure.proportion&quot;: &quot;0.15&quot;,
+    &quot;project.structure.side.proportion&quot;: &quot;0.23448277&quot;,
+    &quot;settings.editor.selected.configurable&quot;: &quot;preferences.pluginManager&quot;,
+    &quot;vue.rearranger.settings.migration&quot;: &quot;true&quot;
+  }
+}</component>
+  <component name="RecentsManager">
+    <key name="CreateClassDialog.RecentsKey">
+      <recent name="org.educa.dao" />
+      <recent name="org.educa.exceptions" />
+      <recent name="org.educa.service" />
+    </key>
+    <key name="CopyFile.RECENT_KEYS">
+      <recent name="C:\Users\manue\Nextcloud\CLASE\02-DAM-AD\Examenes\2-Ev. Ordinaria\Diurno\RA5\ad-eo-ra5-examen-sol\src\test\java\org\educa" />
+      <recent name="C:\Users\manue\Nextcloud\CLASE\02-DAM-AD\Examenes\EO\Diurno\RA5\ad-eo-ra5-examen-sol\src\test\resources" />
+      <recent name="C:\Users\manue\Nextcloud\CLASE\02-DAM-AD\Examenes\RA5\ad-ut5-practica-sol\src\main\java\org\educa" />
+      <recent name="C:\Desarrollo\Docencia\Workspace\AD\2024-25\UT5\ad-ut5-practica-sol\src\test\resources" />
+      <recent name="C:\Desarrollo\Docencia\Workspace\AD\2024-25\UT5\ad-ut5-practica-sol\src\test" />
+    </key>
+    <key name="CreateTestDialog.Recents.Supers">
+      <recent name="" />
+    </key>
+    <key name="CreateTestDialog.RecentsKey">
+      <recent name="org.educa.service" />
+    </key>
+    <key name="CopyClassDialog.RECENTS_KEY">
+      <recent name="org.educa.setup" />
+      <recent name="org.educa.service" />
+      <recent name="org.educa" />
+      <recent name="org.educa.entity" />
+      <recent name="org.example" />
+    </key>
+  </component>
+  <component name="RunManager">
+    <configuration name="Main" type="Application" factoryName="Application" temporary="true" nameIsGenerated="true">
+      <option name="MAIN_CLASS_NAME" value="org.educa.Main" />
+      <module name="ad-eo-ra5-examen" />
+      <extension name="coverage">
+        <pattern>
+          <option name="PATTERN" value="org.educa.*" />
+          <option name="ENABLED" value="true" />
+        </pattern>
+      </extension>
+      <method v="2">
+        <option name="Make" enabled="true" />
+      </method>
+    </configuration>
+    <configuration default="true" type="docker-deploy" factoryName="docker-compose.yml" temporary="true">
+      <deployment type="docker-compose.yml">
+        <settings />
+      </deployment>
+      <method v="2" />
+    </configuration>
+    <recent_temporary>
+      <list>
+        <item itemvalue="Application.Main" />
+      </list>
+    </recent_temporary>
+  </component>
+  <component name="SpellCheckerSettings" RuntimeDictionaries="0" Folders="0" CustomDictionaries="0" DefaultDictionary="application-level" UseSingleDictionary="true" transferred="true" />
+  <component name="TaskManager">
+    <task active="true" id="Default" summary="Default task">
+      <changelist id="0a231334-32e4-40c1-9335-cdd063e03ea5" name="Changes" comment="" />
+      <created>1738773232320</created>
+      <option name="number" value="Default" />
+      <option name="presentableId" value="Default" />
+      <updated>1738773232320</updated>
+      <workItem from="1738773233389" duration="7932000" />
+      <workItem from="1738830694144" duration="8504000" />
+      <workItem from="1738841391888" duration="860000" />
+      <workItem from="1739123244007" duration="616000" />
+      <workItem from="1739190514492" duration="5354000" />
+      <workItem from="1739531995235" duration="328000" />
+      <workItem from="1739952942517" duration="14803000" />
+      <workItem from="1740038635149" duration="5661000" />
+      <workItem from="1740067481488" duration="5220000" />
+      <workItem from="1740130131506" duration="3293000" />
+      <workItem from="1740235784186" duration="3148000" />
+      <workItem from="1740238997426" duration="354000" />
+      <workItem from="1740418301940" duration="2110000" />
+      <workItem from="1740476091686" duration="3403000" />
+      <workItem from="1740481479647" duration="696000" />
+      <workItem from="1740495884089" duration="2000" />
+      <workItem from="1740651718339" duration="787000" />
+      <workItem from="1740653874264" duration="650000" />
+      <workItem from="1740671392250" duration="3638000" />
+      <workItem from="1741023540241" duration="9000" />
+      <workItem from="1741023588716" duration="3166000" />
+      <workItem from="1741252193630" duration="1007000" />
+      <workItem from="1741258333201" duration="1598000" />
+      <workItem from="1741282949251" duration="89000" />
+      <workItem from="1741531754813" duration="2820000" />
+      <workItem from="1741534746313" duration="27000" />
+      <workItem from="1741534920007" duration="84000" />
+      <workItem from="1741633486254" duration="1377000" />
+      <workItem from="1747997851157" duration="654000" />
+      <workItem from="1778926200370" duration="3865000" />
+      <workItem from="1778931895745" duration="4075000" />
+      <workItem from="1779187346582" duration="159000" />
+    </task>
+    <servers />
+  </component>
+  <component name="TypeScriptGeneratedFilesManager">
+    <option name="version" value="3" />
+  </component>
+  <component name="XDebuggerManager">
+    <breakpoint-manager>
+      <breakpoints>
+        <line-breakpoint enabled="true" type="java-line">
+          <url>file://$PROJECT_DIR$/src/test/java/org/educa/setup/BeforeAllTestsExtension.java</url>
+          <line>63</line>
+          <option name="timeStamp" value="2" />
+        </line-breakpoint>
+      </breakpoints>
+    </breakpoint-manager>
+  </component>
+</project>
